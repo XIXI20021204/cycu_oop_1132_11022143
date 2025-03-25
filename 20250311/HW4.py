7540cabe9015e4742d6d39a20486bd338f7ebda4
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-03-25 18:13:21 +0800

    Create HW4.py

---
+++ 20250311/HW4.py
@@ -0,0 +1,8 @@
+print(reversed('parrot'))
+print(list(reversed('parrot')))
+print(''.join(reversed('parrot')))
+print(def reverse_word(word):
+    return ''.join(reversed(word)))
+print(for word in word_list:
+    if len(word) >= 7 and is_palindrome(word):
+        print(word))
