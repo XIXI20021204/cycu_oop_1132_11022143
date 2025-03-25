02b626eb292f0b82740e8fa6b13b60e7ee3fbcf2
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-03-25 19:45:46 +0800

    c

--- 20250325/HW2.py
+++ 20250325/HW2.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# 設置中文字體
+font_path = 'C:/Windows/Fonts/msjh.ttc'  # 微軟正黑體字體路徑
+font_prop = FontProperties(fname=font_path)
 
 # 讀取 CSV 檔案
 file_path = r'c:\Users\User\Documents\GitHub\cycu_oop_1132_11022143\20250325\gold.csv'
@@ -10,20 +15,20 @@ df['資料日期'] = pd.to_datetime(df['資料日期'], format='%Y%m%d')
 
 # 選取需要的資料
 dates = df['資料日期']
-cash_1 = df['BUY']
-cash_2 = df['SELL']
+cash_1 = df['現金.BUY']
+cash_2 = df['現金.SELL']
 
 # 繪製折線圖
 plt.figure(figsize=(10, 5))
-plt.plot(dates, cash_1, label='BUY', marker='o')
-plt.plot(dates, cash_2, label='SELL', marker='o')
+plt.plot(dates, cash_1, label='現金.BUY', marker='o')
+plt.plot(dates, cash_2, label='現金.SELL', marker='o')
 
 # 設定圖表標題和標籤
-plt.title('現金匯率折線圖')
-plt.xlabel('資料日期')
-plt.ylabel('現金匯率')
-plt.xticks(rotation=45)
-plt.legend()
+plt.title("現金匯率折線圖", fontproperties=font_prop)
+plt.xlabel('日期', fontproperties=font_prop)
+plt.ylabel('現金匯率', fontproperties=font_prop)
+plt.xticks(rotation=45, fontproperties=font_prop)
+plt.legend(prop=font_prop)
 
 # 顯示圖表
 plt.tight_layout()
