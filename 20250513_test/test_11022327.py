d5b9b823ab18c884dff0ba51b1f1fadb76e78e4e
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-05-13 20:31:09 +0800

    Update test_11022327.py

--- 20250513_test/test_11022327.py
+++ 20250513_test/test_11022327.py
@@ -1,6 +1,7 @@
-from cycu11022327.ebus_map import taipei_route_list, taipei_route_info
+import cycu11022327
 
-if __name__ == "__main__":
-    stop_ids = get_bus_info_go('0161000900')
-    for stop_id in stop_ids:
-        print(f"stop_id: {stop_id}")
+from cycu11022327.ebus_map import get_bus_info_go
+
+stop_ids = get_bus_info_go('0161000900')
+for stop_id in stop_ids:
+    print(f"stop_id: {stop_id}")
