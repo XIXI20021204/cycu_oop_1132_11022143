deabc6b20c976b86429c9da84e7db0401c9e227a
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-05-13 20:19:34 +0800

    C

---
+++ 20250513_test/test_11022327.py
@@ -0,0 +1,6 @@
+from cycu25305.ebus_map import taipei_route_list, taipei_route_info
+
+if __name__ == "__main__":
+    stop_ids = get_bus_info_go('0161000900')
+    for stop_id in stop_ids:
+        print(f"stop_id: {stop_id}")
