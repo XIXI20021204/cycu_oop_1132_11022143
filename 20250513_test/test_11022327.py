bcb257d5937b93727817d2a7647a957f69e534cc
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-05-13 20:34:59 +0800

    C

--- 20250513_test/test_11022327.py
+++ 20250513_test/test_11022327.py
@@ -1,6 +1,6 @@
-import cycu11022327
+import m11022327
 
-from cycu11022327.ebus_map import get_bus_info_go
+from m11022327.ebus_map import get_bus_info_go
 
 stop_ids = get_bus_info_go('0161000900')
 for stop_id in stop_ids:
