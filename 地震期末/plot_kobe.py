958c8eae5287d3f813464cc574a0ec355c44a51b
JIAN-JIE,HUANG <a0965349199@gmail.com>
2025-06-01 20:21:34 +0800

    C

---
+++ 地震期末/plot_kobe.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']  # 設定中文字型
+plt.rcParams['axes.unicode_minus'] = False  # 正確顯示負號
+
+# 讀取資料，跳過第一行標頭
+data = np.loadtxt(r'c:\Users\a0965\OneDrive\文件\GitHub\cycu_oop_1132_11022143\地震期末\Kobe.txt', skiprows=1)
+time = data[:, 0]
+acc = data[:, 1]
+
+plt.figure(figsize=(12, 5))
+plt.plot(time, acc, label='Kobe Acceleration')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (g)')
+plt.title('日本神戶地震地表加速歷時圖')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.savefig('kobe_acc_time_history.jpg', format='jpg', dpi=300)
+plt.show()
